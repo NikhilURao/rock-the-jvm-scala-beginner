8f966925b1c1f2ec609434c8bb14782e18dcc84b
nurao <nikhilurao@gmail.com>
2019-01-25 17:43:11 +0530

    Mod

--- src/exercises/FunctionsExercise.scala
+++ src/exercises/FunctionsExercise.scala
@@ -14,28 +14,30 @@ object FunctionsExercise extends App {
   //2.Factorial Function
 
   def aFactorialFunction(num: Int): Int = {
-    if (num == 1) num
+    if (num <=0) 1
     else num * aFactorialFunction(num - 1)
   }
-  println(aFactorialFunction(4))
+  println(aFactorialFunction(0))
 
   //3.A Fibonacci Function
 
   def aFibonacciFunction(n : Int) : Int = {
 
-    if (n==1) 1
-    else 1 + aFibonacciFunction(n-1)
+    if (n <=2) 1
+    else aFibonacciFunction(n-1) + aFibonacciFunction(n-2)
   }
-println(aFibonacciFunction(n=3))
+println(aFibonacciFunction(n=8))
 
 
   //4.Test if a number is a prime
 
   def testifprime (num : Int) : Boolean = {
-
-    if (num % 2 == 0) false else true
+    def isprimeuntil(x : Int) : Boolean =
+      if (x <= 1) true
+      else num % x != 0 && isprimeuntil (x - 1)
+  isprimeuntil(num / 2)
   }
-  println(testifprime (13))
+  println(testifprime (2000))
 }
 
 
