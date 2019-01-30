8c5a1e75e065e4c11e66d3e55960d0ba6e2d1aef
nurao <nikhilurao@gmail.com>
2019-01-30 12:57:39 +0530

    mod

--- src/lectures/part1basics/CBNvsCBV.scala
+++ src/lectures/part1basics/CBNvsCBV.scala
@@ -2,14 +2,15 @@ package lectures.part1basics
 
 object CBNvsCBV extends App {
 
-  def CalledByValue (x : Long) : Unit = { // CBV the exact value of x is computed before the function evaluates and the same value is used in the function def
-
+  def CalledByValue (x : Long) : Unit = { // CBV the exact value of x is computed before the function evaluates and the same value is used in the function def.
+                                          //value is computed before the call  and same value is used everywhere.
     println("Called By Value"+ x)
     println("Called By Value"+ x)
 
   }
 
   def CalledByName (x : => Long) : Unit = { // CBN the expression of x is passed as is into the function and is evaluated each time.
+                                            //expression is passed as is and is evaluated at every use within
 
     println("Called By Name"+ x)
     println("Called By Name"+ x)
