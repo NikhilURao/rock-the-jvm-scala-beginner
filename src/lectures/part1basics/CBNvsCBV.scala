2b2023dcfc1d23bedbd81910e471d048ab498c9a
nurao <nikhilurao@gmail.com>
2019-01-30 12:53:41 +0530

    mod

---
+++ src/lectures/part1basics/CBNvsCBV.scala
@@ -0,0 +1,27 @@
+package lectures.part1basics
+
+object CBNvsCBV extends App {
+
+  def CalledByValue (x : Long) : Unit = { // CBV the exact value of x is computed before the function evaluates and the same value is used in the function def
+
+    println("Called By Value"+ x)
+    println("Called By Value"+ x)
+
+  }
+
+  def CalledByName (x : => Long) : Unit = { // CBN the expression of x is passed as is into the function and is evaluated each time.
+
+    println("Called By Name"+ x)
+    println("Called By Name"+ x)
+  }
+
+  CalledByValue(System.nanoTime())
+  CalledByName(System.nanoTime())
+
+  def infintie () : Int = 1+ infintie()
+  def printfirst(x : Int, y : => Int) : Unit = println(x) // note that x is CBV and y is CBN
+
+  //printfirst(infintie(),34) // crashes with Stack Overflow error because the infinite() is evaluated first as x is CBV
+  printfirst(34,infintie()) // Doesn't crash becuase the infinite() is not evailuated as y is CBN
+
+}
